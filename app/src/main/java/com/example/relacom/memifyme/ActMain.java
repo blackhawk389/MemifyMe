d1d3557041cf83d26f0c55515b148d684e6c9153
blackhawk389 <sarah.hassan95@yahoo.com>
2017-05-24 13:22:31 +0500

    third commit

--- app/src/main/java/com/example/relacom/memifyme/ActMain.java
+++ app/src/main/java/com/example/relacom/memifyme/ActMain.java
@@ -2,12 +2,28 @@ package com.example.relacom.memifyme;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class ActMain extends AppCompatActivity {
+
+    Button btn;
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_act_main);
+
+        btn = (Button) findViewById(R.id.btn);
+        tv = (TextView) findViewById(R.id.tv);
+
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                tv.setText("New Text");
+            }
+        });
     }
 }
